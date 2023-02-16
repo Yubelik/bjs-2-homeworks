d0d8e4cff1c7bf230f701c01619e35231c1b546c
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-02-16 22:18:54 +0300

    Загружаю 1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let res = b ** 2 -(4 * a * c);
+  let resZero;
+  // console.log(res);
+  if (res < 0){
+    return arr;
+  } else if (res = 0){
+    arr.push(-b/(2*a));
+    // console.log(resZero);
+    return arr;
+  } else if (res > 0){
+    arr.push((-b + Math.sqrt(d) )/(2*a));
+    arr.push((-b - Math.sqrt(d) )/(2*a));
+    return arr;
+  } 
   
-  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+
 }
+
+console.log(solveEquation(1, 5, 4));
