23a03913eaa1e7b0c3dfe122c0c22f519a97f982
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-03-01 16:46:43 +0300

    Делаю объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,23 +1,27 @@
 function Student(name, gender, age) {
-    name,
-    gender,
-    age,
-    marks = []
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
       
 }
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
-
-  
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!Student.hasOwnProperty('marks')){
+        this.marks = this.marks.concat(marks);
+        
+    } 
+    
+    
 }
 
 Student.prototype.getAverage = function () {
-  
+  this.getAverage = this.marks.reduce((partialSum, a) => partialSum + a, 0) / this.marks.length;
+  console.log(this.getAverage)
 }
 
 Student.prototype.exclude = function (reason) {
