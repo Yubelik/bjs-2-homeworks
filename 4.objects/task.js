a6b002079a26813bf3f6eef8bdc142d00616b6dd
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-03-02 22:52:10 +0300

    1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,9 +2,10 @@ function Student(name, gender, age) {
     this.name = name,
     this.gender = gender,
     this.age = age,
-    this.marks = []
-      
+    this.marks = []     
 }
+
+new Student("Петя", "Муж", "20" )
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
@@ -13,6 +14,7 @@ Student.prototype.setSubject = function (subjectName) {
 Student.prototype.addMarks = function (...marks) {
     if (!Student.hasOwnProperty('marks')){
         this.marks = this.marks.concat(marks);
+        // console.log(this.marks);
     } 
 }
 
@@ -24,19 +26,23 @@ Student.prototype.getAverage = function () {
 
 // console.log(this.marks.length);
 
-    if (student.hasOwnProperty('marks') && this.marks.length == 0){
+    if (!Student.hasOwnProperty('marks') && Student.marks.length == 0){
         this.getAverage = 0;
         // console.log(this.getAverage);
     } else {
-        this.getAverage = this.marks.reduce((partialSum, a) => partialSum + a, 0) / this.marks.length;
+        // console.log("has = "+Student.hasOwnProperty('marks'));
+        // console.log("length = "+Student.marks.length);        
+        // console.log("marks = "+this.marks);
+
+        this.getAverage = Student.marks.reduce((partialSum, a) => partialSum + a, 0) / Student.marks.length;
             // console.log(this.getAverage);
             // student.getAverage();
         }
 }
 
-//  let student = new Student();
-//  student.getAverage();
-//  student.addMarks(5, 5, 5);
-  //  student.getAverage();
- //   student.marks = [3, 4, 5];   
- //   student.getAverage();
+//   let student = new Student();
+//   student.getAverage();
+//   student.addMarks(5, 5, 5);
+//     student.getAverage();
+//     student.marks = [3, 4, 5];   
+//     student.getAverage();
