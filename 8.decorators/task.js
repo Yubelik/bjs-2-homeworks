ccf6ef52a20355d43e99121dbfa27f3129ff61cd
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-03-17 22:01:21 +0300

    ДЗ 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,40 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+  function wrapper(...args) {
+      const hash = ???; // получаем правильный хеш c помощью функции md5
+      let objectInCache = cache.find((item) => item == hash ); // ищем элемент, хеш которого равен нашему хешу
+      if (objectInCache) { // если элемент найден
+          console.log("Из кэша: " + objectInCache); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+          return "Из кэша: " + ???;
+      }
+
+      let result = func(...args); // в кеше результата нет — придётся считать
+      cache.push(???) ; // добавляем элемент с правильной структурой
+      if (cache.length > 5) { 
+        ??? // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
+}
+
+
+
+    //const objectInCache = cache.find((item) => тут нужно подумать)`
+    if (!objectInCache){
+        Это можно сделать методом shift() массива
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   
 }
+
+
+const hasingText = "какой-нибудь текст";
+    console.log(md5(hasingText)); // 8d1d3ecc455a4220590e6d27e6c1a267
+    console.log(md5([10, 20, 30])); // 7f49b84d0bbc38e96493718013baace6
