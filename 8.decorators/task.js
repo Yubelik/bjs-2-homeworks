148754ae5b6fe86b791817a15c515af113a19b10
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-03-21 21:54:55 +0300

    Сохранился

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,17 +3,20 @@ function cachingDecoratorNew(func) {
     let cache = [];
 
   function wrapper(...args) {
-      const hash = ???; // получаем правильный хеш c помощью функции md5
+    // debu   gger;
+        md5(args);
+      const hash = md5.create(); // получаем правильный хеш c помощью функции md5
       let objectInCache = cache.find((item) => item == hash ); // ищем элемент, хеш которого равен нашему хешу
       if (objectInCache) { // если элемент найден
           console.log("Из кэша: " + objectInCache); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-          return "Из кэша: " + ???;
+          return "Из кэша: " + objectInCache;
       }
 
+    console.log("Вычисляем значение хеша");   
       let result = func(...args); // в кеше результата нет — придётся считать
-      cache.push(???) ; // добавляем элемент с правильной структурой
+      cache.push(result) ; // добавляем элемент с правильной структурой
       if (cache.length > 5) { 
-        ??? // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+        cache.shift();  // если слишком много элементов в кеше, надо удалить самый старый (первый) 
       }
       console.log("Вычисляем: " + result);
       return "Вычисляем: " + result;  
@@ -23,11 +26,11 @@ function cachingDecoratorNew(func) {
 
 
 
-    //const objectInCache = cache.find((item) => тут нужно подумать)`
-    if (!objectInCache){
-        Это можно сделать методом shift() массива
-    }
-}
+    // //const objectInCache = cache.find((item) => тут нужно подумать)`
+    // if (!objectInCache){
+    //     //Это можно сделать методом shift() массива
+    // }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
@@ -38,3 +41,6 @@ function debounceDecoratorNew(func, delay) {
 const hasingText = "какой-нибудь текст";
     console.log(md5(hasingText)); // 8d1d3ecc455a4220590e6d27e6c1a267
     console.log(md5([10, 20, 30])); // 7f49b84d0bbc38e96493718013baace6
+
+
+
