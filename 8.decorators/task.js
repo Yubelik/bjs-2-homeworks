426993db97f9bf5459c0f56192695ad8d4f87ac5
Yubelik <89403380+Yubelik@users.noreply.github.com>
2023-03-28 23:33:05 +0300

    сохранение

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,20 +1,22 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-    let cache = [];
+    let cache = {};
 
   function wrapper(...args) {     
-      const hash = func(...args)//md5(args); // получаем правильный хеш c помощью функции md5
-      let objectInCache = cache.find((item) => item == hash ); // ищем элемент, хеш которого равен нашему хешуs
+      const hash = md5(args);//func(...args)//md5(args); // получаем правильный хеш c помощью функции md5
+      let objectInCache = cache.find((item) => item.hash == hash ); // ищем элемент, хеш которого равен нашему хешуs
     
-      if (objectInCache) { // если элемент найден
+      if (objectInCache && objectInCache !== undefined) { // если элемент найден
           console.log("Из кэша: " + objectInCache); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-          return "Из кэша: " + objectInCache;
+          
+          return "Из кэша: " + cache[objectInCache];
       }
 
     console.log("Вычисляем значение хеша");   
+      
       let result = func(...args); // в кеше результата нет — придётся считать
       
-      cache.push(result) ; // добавляем элемент с правильной структурой
+      cache.push({hash:result}) ; // добавляем элемент с правильной структурой
       
       if (cache.length > 5) { 
         cache.shift();  // если слишком много элементов в кеше, надо удалить самый старый (первый) 
@@ -25,48 +27,31 @@ function cachingDecoratorNew(func) {
   return wrapper;
 }
 
-
+const addAndMultiply = (a, b, c) => (a + b) * c;
+const upgraded = cachingDecoratorNew(addAndMultiply);
+upgraded(1, 2, 3); // вычисляем: 9
+upgraded(1, 2, 3); // из кэша: 9
 
     
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
+// //Задача № 2
+// function debounceDecoratorNew(func, delay) {
+//   wrapper.count=0;
+//   wrapper.allCount=0;
+//   let timeoutID = null;
+//     function wrapper(...args){
+//       wrapper.count++;
+//         if (timeoutID=null){
+//             wrapper.allCount++;
+//             clearTimeout(timeoutID);
+//         }
+//         timeoutID = setTimeout(() => {
+//             wrapper.count++;
+//             func(...args)}, delay);
+//     }
     
     
-    function wrapper(...args){
-        //  console.log("timeoutID = "+timeoutID);
-        
-        if (timeoutID){
-            console.log("Удаляем timeoutID");
-            wrapper.allCount++;
-            clearTimeout(timeoutID);
-        }
-        // console.log("создаем новый timeoutID");
-        timeoutID = setTimeout(() => {
-            // timeoutID = null;
-            wrapper.count++;
-            func(...args)}, delay);
-    }
-    
-    wrapper.count=0;
-    wrapper.allCount=0;
-    let timeoutID = null;
-    return wrapper;
-}
-
-
-// const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
-// const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
-// setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован, так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
-// setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано, так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
-// setTimeout(() => upgradedSendSignal(3, 900), 900); // проигнорировано, так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
-// setTimeout(() => upgradedSendSignal(4, 1200), 1200); // проигнорировано, так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
-// setTimeout(() => upgradedSendSignal(5, 2300), 2300); // Сигнал отправлен, так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
-// setTimeout(() => upgradedSendSignal(6, 4400), 4400); // проигнорировано, так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
-// setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
-// setTimeout(() => {
-//   console.log(upgradedSendSignal.count); // было выполнено 3 отправки сигнала
-//   console.log(upgradedSendSignal.allCount); // было выполнено 6 вызовов декорированной функции
-// }, 7000);
+//     return wrapper;
+// }
 
 
 
